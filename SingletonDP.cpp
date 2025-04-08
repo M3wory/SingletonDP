b18bd49880f81e934c6c964d7fe0e504ee68732a
M3wory <orand9@yandex.ru>
2025-04-08 12:13:33 +0300

    Добавьте файлы проекта.

---
+++ SingletonDP.cpp
@@ -0,0 +1,12 @@
+﻿// SingletonDP.cpp: определяет точку входа для приложения.
+//
+
+#include "SingletonDP.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
